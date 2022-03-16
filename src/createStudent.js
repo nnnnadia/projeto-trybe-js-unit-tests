26c29a837f6c4eeec9df3a65f88e31e2a0157720
nnnnadia <tristaonadia@gmail.com>
2022-03-16 11:53:05 -0300

    Corrige erros ESLint do arquivo do requisito 5.

--- src/createStudent.js
+++ src/createStudent.js
@@ -17,11 +17,9 @@
 
 const createStudent = (name) => {
   const estudante = {
-    name: name,
-    feedback: () => {
-      return 'Eita pessoa boa!'
-    }
-  }
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  };
   return estudante;
 };
 
